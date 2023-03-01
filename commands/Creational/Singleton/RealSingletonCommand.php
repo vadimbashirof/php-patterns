c66f7a59dadf166692abae4daa71eaebf05ff15a
vadimbashirof <vadimbashirof@gmail.com>
2023-03-01 18:55:30 +0300

    RealSingleton

---
+++ commands/Creational/Singleton/RealSingletonCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Commands\Creational\Singleton;
+
+use Patterns\Creational\Singleton\RealSingleton\Config;
+use Patterns\Creational\Singleton\RealSingleton\Logger;
+use Patterns\Creational\Singleton\Singleton;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class RealSingletonCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('RealSingletonCommand');
+    }
+
+    /**
+     * Клиентский код.
+     */
+    public function clientCode()
+    {
+        Logger::log("Started!");
+
+        // Сравниваем значения одиночки-Логгера.
+        $l1 = Logger::getInstance();
+        $l2 = Logger::getInstance();
+        if ($l1 === $l2) {
+            Logger::log("Logger has a single instance.");
+        } else {
+            Logger::log("Loggers are different.");
+        }
+
+        // Проверяем, как одиночка-Конфигурация сохраняет данные...
+        $config1 = Config::getInstance();
+        $login = "test_login";
+        $password = "test_password";
+        $config1->setValue("login", $login);
+        $config1->setValue("password", $password);
+        // ...и восстанавливает их.
+        $config2 = Config::getInstance();
+        if ($login == $config2->getValue("login") &&
+            $password == $config2->getValue("password")
+        ) {
+            Logger::log("Config singleton also works fine.");
+        }
+
+        Logger::log("Finished!");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->clientCode();
+
+        return Command::SUCCESS;
+    }
+}
--- tests/Creational/SingletonTest.php
+++ tests/Creational/SingletonTest.php
@@ -1,7 +1,8 @@
 <?php
 
-
 use Patterns\Creational\Builder\CarBuilder\Manual;
+use Patterns\Creational\Singleton\RealSingleton\Config;
+use Patterns\Creational\Singleton\RealSingleton\Logger;
 use Patterns\Creational\Singleton\Singleton;
 use PHPUnit\Framework\TestCase;
 
@@ -15,5 +16,26 @@ class SingletonTest extends TestCase
         $this->assertInstanceOf(Singleton::class, $s1);
         $this->assertInstanceOf(Singleton::class, $s2);
         $this->assertSame($s1, $s2);
+    }
+
+
+    public function testRealSingleton()
+    {
+        $l1 = Logger::getInstance();
+        $l2 = Logger::getInstance();
+
+        $this->assertInstanceOf(Singleton::class, $l1);
+        $this->assertInstanceOf(Singleton::class, $l2);
+        $this->assertSame($l1, $l2);
+
+        $config1 = Config::getInstance();
+        $config1->setValue("login", "test_login");
+        $config1->setValue("password", "test_password");
+
+        $config2 = Config::getInstance();
+
+        $this->assertSame($config1, $config2);
+        $this->assertSame($config1->getValue("login"), $config2->getValue("login"));
+        $this->assertSame($config1->getValue("password"), $config2->getValue("password"));
     }
 }
